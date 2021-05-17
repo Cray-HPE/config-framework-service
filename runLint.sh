a80629a13f27c2152b6501c73f6facd57ebe33d9
Mitch Harding <mitchell.harding@hpe.com>
2021-05-17 14:43:59 -0500

    CASMCMS-7057: Add MIT license to appropriate files; Add linter for same; Update README

--- runLint.sh
+++ runLint.sh
@@ -1,103 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
+# Copyright 2019-2021 Hewlett Packard Enterprise Development LP
 #
-# Copyright 2019, Cray Inc.  All Rights Reserved.
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
 #
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# (MIT License)
 
-# Very simple scanner for files missing copyrights
-
-# Extensions to check
-CODE_EXTENSIONS="py sh"
-
-# Additional files to check, uses exact match
-EXTRA_FILES="Dockerfile api/openapi.yaml"
-
-WHITELIST_FILES="src/server/cray/cfs/api/models/config_framework_session.py
-src/server/cray/cfs/api/models/config_framework_session_ansible.py
-src/server/cray/cfs/api/models/config_framework_session_create.py
-src/server/cray/cfs/api/models/config_framework_session_repo.py
-src/server/cray/cfs/api/models/config_framework_session_status.py
-src/server/cray/cfs/api/models/config_framework_session_status_artifacts.py
-src/server/cray/cfs/api/models/config_framework_session_status_session.py
-src/server/cray/cfs/api/models/config_framework_session_status_targets.py
-src/server/cray/cfs/api/models/link.py
-src/server/cray/cfs/api/models/problem_details.py
-src/server/cray/cfs/api/models/target_spec_section.py
-src/server/cray/cfs/api/models/target_spec_section_groups.py
-src/server/cray/cfs/api/controllers/sessions_controller.py
-src/server/README.md
-src/server/test-requirements.txt
-src/server/requirements.txt
-src/server/cray/cfs/api/util.py
-src/server/cray/cfs/api/typing_utils.py
-src/server/cray/cfs/api/controllers/__init__.py
-src/server/cray/cfs/api/controllers/security_controller_.py
-src/server/cray/cfs/api/models/__init__.py
-src/server/cray/cfs/api/models/base_model_.py
-src/server/cray/cfs/api/openapi/openapi.yaml
-src/server/.gitignore
-src/server/Dockerfile
-src/server/.dockerignore
-src/server/setup.py
-src/server/tox.ini
-src/server/git_push.sh
-src/server/.travis.yml
-src/server/cray/cfs/api/encoder.py
-src/server/cray/cfs/api/test/__init__.py
-src/server/.openapi-generator/VERSION"
-
-FAIL=0
-
-function scan_file {
-    echo -n "Scanning $1... "
-    # skip empty files
-    if [ -s $1 ]; then
-        grep -q "Copyright" $1
-        if [ $? -ne 0 ]; then
-            echo "missing copyright headers"
-            return 1
-        fi
-    fi
-    echo "OK"
-    return 0
-}
-
-function list_include_item {
-  local list="$1"
-  local item="$2"
-  if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]] ; then
-    # yes, list include item
-    result=0
-  else
-    result=1
-  fi
-  return $result
-}
-
-# Scan extentions
-for CE in ${CODE_EXTENSIONS}
-do
-    for F in `git ls-files "*.${CE}"`
-    do
-        if ! list_include_item "$WHITELIST_FILES" "$F"; then
-            scan_file ${F}
-            if [ $? -ne 0 ]; then
-                FAIL=1
-            fi
-        fi
-    done
-done
-
-# Do the listed extra files
-for F in ${EXTRA_FILES}
-do
-    scan_file ${F}
-done
-
-if [ ${FAIL} -eq 0 ]; then
-    echo "All scanned code passed"
-else
-    echo "Some code is missing copyright, see list above"
-fi
-
-exit ${FAIL}
+./install_cms_meta_tools.sh || exit 1
+RC=0
+./cms_meta_tools/copyright_license_check/copyright_license_check.sh || RC=1
+./cms_meta_tools/go_lint/go_lint.sh || RC=1
+rm -rf ./cms_meta_tools
+exit $RC
