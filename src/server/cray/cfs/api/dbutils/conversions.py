180a293a415c8515a7d9eaa4d1c3f1e241f66022
Mitch Harding <mitchell.harding@hpe.com>
2025-10-14 17:20:15 -0400

    Apply suggestions from code review
    (cherry picked from commit afe36743da5885160232a757221cf118851cab8c)

--- src/server/cray/cfs/api/dbutils/conversions.py
+++ src/server/cray/cfs/api/dbutils/conversions.py
@@ -30,14 +30,21 @@ from typing import Any
 
 from cray.cfs.api.models.base_model import Model as BaseModel
 
-from .typing import JsonDict
+from .typing import JsonData, JsonDict
 
 
 def convert_data_to_v2(data: JsonDict, model_type: type[BaseModel]) -> JsonDict:
     """
+    Field names in CFS v2 are in camelCase and in CFS v3 they are in snake_case.
+    This function takes dictionary data in the CFS v3 format, and returns the CFS v2 equivalent.
+    The specified model_type is the auto-generated model class for the CFS v2 schema for the
+    data being converted. These model classes are generated at built time by the OpenAPI Generator.
+    See the Dockerfile for details.
+
     When exporting from a model with to_dict, all keys are in snake_case.  However the model
-        contains the information on the keys in the api spec.  This gives the ability to make the
-        data match the given model/spec, which is useful when translating between the v2 and v3 api
+    contains the information on the keys in the api spec.  This gives the ability to make the
+    data match the given model/spec, which is useful when translating between the v2 and v3 api
+
     Data must start in the v3 format exported by model().to_dict()
     """
     result = {}
@@ -49,7 +56,7 @@ def convert_data_to_v2(data: JsonDict, model_type: type[BaseModel]) -> JsonDict:
     return result
 
 
-def _convert_data_to_v2(data: JsonDict, data_type: Any) -> JsonDict:
+def _convert_data_to_v2(data: JsonData, data_type: Any) -> JsonData:
     if not isinstance(data_type, type):
         # Special case where the data_type is a "typing" object.  e.g typing.Dict
         if not data:
@@ -69,10 +76,17 @@ def _convert_data_to_v2(data: JsonDict, data_type: Any) -> JsonDict:
 
 def convert_data_from_v2(data: JsonDict, model_type: type[BaseModel]) -> JsonDict:
     """
+    Field names in CFS v2 are in camelCase and in CFS v3 they are in snake_case.
+    This function takes dictionary data in the CFS v2 format, and returns the CFS v3 equivalent.
+    The specified model_type is the auto-generated model class for the CFS v2 schema for the
+    data being converted. These model classes are generated at built time by the OpenAPI Generator.
+    See the Dockerfile for details.
+
     When exporting from a model with to_dict, all keys are in snake_case.  However the model
-        contains the information on the keys in the api spec.  This gives the ability to make the
-        data match the given model/spec, which is useful when translating between the v2 and v3 api
-    Data must start in the v3 format exported by model().to_dict()
+    contains the information on the keys in the api spec.  This gives the ability to make the
+    data match the given model/spec, which is useful when translating between the v2 and v3 api
+
+    Data must start in the v2 format exported by model().to_dict()
     """
     result = {}
     model = model_type()
@@ -83,7 +97,7 @@ def convert_data_from_v2(data: JsonDict, model_type: type[BaseModel]) -> JsonDic
     return result
 
 
-def _convert_data_from_v2(data: JsonDict, data_type: Any) -> JsonDict:
+def _convert_data_from_v2(data: JsonData, data_type: Any) -> JsonData:
     if not isinstance(data_type, type):
         # Special case where the data_type is a "typing" object.  e.g typing.Dict
         if not data:
