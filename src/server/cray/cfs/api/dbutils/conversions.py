ccd6d800700edfd4f61f032b7efd66f78bcf8cc9
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2025-10-14 17:28:11 -0400

    Rename variables and arguments to clarify logic
    (cherry picked from commit cd03e0eec3d46b73ad1f47d36753e67896903890)

--- src/server/cray/cfs/api/dbutils/conversions.py
+++ src/server/cray/cfs/api/dbutils/conversions.py
@@ -33,7 +33,7 @@ from cray.cfs.api.models.base_model import Model as BaseModel
 from .typing import JsonData, JsonDict
 
 
-def convert_data_to_v2(data: JsonDict, model_type: type[BaseModel]) -> JsonDict:
+def convert_data_to_v2(v3_data: JsonDict, v2_model_type: type[BaseModel]) -> JsonDict:
     """
     Field names in CFS v2 are in camelCase and in CFS v3 they are in snake_case.
     This function takes dictionary data in the CFS v3 format, and returns the CFS v2 equivalent.
@@ -47,34 +47,34 @@ def convert_data_to_v2(data: JsonDict, model_type: type[BaseModel]) -> JsonDict:
 
     Data must start in the v3 format exported by model().to_dict()
     """
-    result = {}
-    model = model_type()
-    for attribute, attribute_key in model.attribute_map.items():
-        if attribute in data:
-            data_type = model.openapi_types[attribute]
-            result[attribute_key] = _convert_data_to_v2(data[attribute], data_type)
-    return result
+    v2_data = {}
+    v2_model = v2_model_type()
+    for attribute, attribute_key in v2_model.attribute_map.items():
+        if attribute in v3_data:
+            data_type = v2_model.openapi_types[attribute]
+            v2_data[attribute_key] = _convert_data_to_v2(v3_data[attribute], data_type)
+    return v2_data
 
 
-def _convert_data_to_v2(data: JsonData, data_type: Any) -> JsonData:
+def _convert_data_to_v2(v3_data: JsonData, data_type: Any) -> JsonData:
     if not isinstance(data_type, type):
         # Special case where the data_type is a "typing" object.  e.g typing.Dict
-        if not data:
-            return data
+        if not v3_data:
+            return v3_data
         if data_type.__origin__ == list:
             return [_convert_data_to_v2(item_data, data_type.__args__[0])
-                    for item_data in data]
+                    for item_data in v3_data]
         if data_type.__origin__ == dict:
             return {key: _convert_data_to_v2(item_data, data_type.__args__[1])
-                    for key, item_data in data.items()}
+                    for key, item_data in v3_data.items()}
     elif issubclass(data_type, BaseModel):
-        if not data:
-            data = {}
-        return convert_data_to_v2(data, data_type)
-    return data
+        if not v3_data:
+            v3_data = {}
+        return convert_data_to_v2(v3_data, data_type)
+    return v3_data
 
 
-def convert_data_from_v2(data: JsonDict, model_type: type[BaseModel]) -> JsonDict:
+def convert_data_from_v2(v2_data: JsonDict, v2_model_type: type[BaseModel]) -> JsonDict:
     """
     Field names in CFS v2 are in camelCase and in CFS v3 they are in snake_case.
     This function takes dictionary data in the CFS v2 format, and returns the CFS v3 equivalent.
@@ -88,28 +88,28 @@ def convert_data_from_v2(data: JsonDict, model_type: type[BaseModel]) -> JsonDic
 
     Data must start in the v2 format exported by model().to_dict()
     """
-    result = {}
-    model = model_type()
-    for attribute_key, attribute in model.attribute_map.items():
-        if attribute in data:
-            data_type = model.openapi_types[attribute_key]
-            result[attribute_key] = _convert_data_from_v2(data[attribute], data_type)
-    return result
+    v3_data = {}
+    v2_model = v2_model_type()
+    for attribute_key, attribute in v2_model.attribute_map.items():
+        if attribute in v2_data:
+            data_type = v2_model.openapi_types[attribute_key]
+            v3_data[attribute_key] = _convert_data_from_v2(v2_data[attribute], data_type)
+    return v3_data
 
 
-def _convert_data_from_v2(data: JsonData, data_type: Any) -> JsonData:
+def _convert_data_from_v2(v2_data: JsonData, data_type: Any) -> JsonData:
     if not isinstance(data_type, type):
         # Special case where the data_type is a "typing" object.  e.g typing.Dict
-        if not data:
-            return data
+        if not v2_data:
+            return v2_data
         if data_type.__origin__ == list:
             return [_convert_data_from_v2(item_data, data_type.__args__[0])
-                    for item_data in data]
+                    for item_data in v2_data]
         if data_type.__origin__ == dict:
             return {key: _convert_data_from_v2(item_data, data_type.__args__[1])
-                    for key, item_data in data.items()}
+                    for key, item_data in v2_data.items()}
     elif issubclass(data_type, BaseModel):
-        if not data:
-            data = {}
-        return convert_data_from_v2(data, data_type)
-    return data
+        if not v2_data:
+            v2_data = {}
+        return convert_data_from_v2(v2_data, data_type)
+    return v2_data
