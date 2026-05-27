c34b91c75cc69953d46e0db3c0283d6f49014e4b
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2026-05-27 13:54:01 -0400

    CASMCMS-9576: dbutils: Create redis_pipeline decorator; modify delete_all, patch to use it
    (cherry picked from commit b19b9a7f962cc3469d03e734d8f1e25dcd1b45bb)

--- src/server/cray/cfs/api/dbutils/decorators.py
+++ src/server/cray/cfs/api/dbutils/decorators.py
@@ -79,23 +79,54 @@ class HasTooBusyExceptionMethod(Protocol):
         pass
 
 
-C = TypeVar("C", bound="HasTooBusyExceptionMethod")
+C1 = TypeVar("C1", bound="HasTooBusyExceptionMethod")
 
 
 def convert_db_watch_errors(
-    method: Callable[Concatenate[C, P], R]
-) -> Callable[Concatenate[C, P], R]:
+    method: Callable[Concatenate[C1, P], R]
+) -> Callable[Concatenate[C1, P], R]:
     """
     Decorator to put around DBWrapper methods that catches redis WatchError
     and raises DBTooBusyError from it
     """
 
     @functools.wraps(method)
-    def wrapper(self: C, /, *args: P.args, **kwargs: P.kwargs) -> R:
+    def wrapper(self: C1, /, *args: P.args, **kwargs: P.kwargs) -> R:
         try:
             return method(self, *args, **kwargs)
         except redis.exceptions.WatchError as err:
             LOGGER.debug(err)
+            # Because C is bounded by the HasTooBusyExceptionMethod protocol, we know that
+            # self.too_busy_exception() must exist and return a DBTooBusyError
             raise self.too_busy_exception() from err
+
+    return wrapper
+
+
+class HasRedisClient(Protocol):
+    """
+    Protocol for classes that have a Redis client instance variable named 'client'
+    (e.g. DBWrapper)
+    """
+    client: redis.client.Redis
+
+
+C2 = TypeVar("C2", bound="HasRedisClient")
+
+
+def redis_pipeline(
+    method: Callable[Concatenate[C2, redis.client.Pipeline, P], R]
+) -> Callable[Concatenate[C2, P], R]:
+    """
+    Decorator to put around DBWrapper methods that provides a Redis pipeline as the
+    first argument to the function (inside of a context manager).
+    """
+
+    @functools.wraps(method)
+    def wrapper(self: C2, /, *args: P.args, **kwargs: P.kwargs) -> R:
+        # Because C is bounded by the HasRedisClient protocol, we know that
+        # self.client is a redis.client.Redis object
+        with self.client.pipeline() as pipe:
+            return method(self, pipe, *args, **kwargs)
 
     return wrapper
